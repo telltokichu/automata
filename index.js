a3e57e3db905bb5d9cc1f16b7cf92ac5894d56de
kishoreag <kishoreag@cloudix.io>
2023-10-10 20:04:51 +0530

    in progress

--- index.js
+++ index.js
@@ -9,25 +9,42 @@ import puppeteer from "puppeteer";
         document.body.style.width = '100%';
     });
     await page.goto('https://www.moneycontrol.com/markets/indian-indices/');
-
-
-    const data = await page.evaluate(() => {
-        const thr = document.querySelector('#indicesTableData thead tr');
-        const thEl = Array.from(thr.querySelectorAll('th'));
-        const keys = thEl.map((td) => td.textContent.trim());
-
-        const tbody = document.querySelector('#indicesTableData tbody');
-        const allRows = Array.from(tbody.querySelectorAll('tr'));
-        let allData = []
-        allRows.map((tr) => {
-            const tds = Array.from(tr.querySelectorAll('td'));
-            let trow = {}
-            tds.map((td, index) => {
-                trow[`${keys[index]}`] = td.textContent.trim()
+    // await page.goto('https://pptr.dev/api/puppeteer.page.waitfornavigation/');
+    const getTableData = async () => {
+        console.log("vanten")
+        const data = await page.evaluate(() => {
+            let allData = []
+            const table = document.querySelector('#indicesTableData');
+            const headers = Array.from(table.querySelectorAll('thead th')).map((td) => td.textContent.trim());
+            const rows = Array.from(table.querySelectorAll('tbody tr'));
+            rows.map((tr) => {
+                const cells = Array.from(tr.querySelectorAll('td'));
+                let trow = {}
+                cells.map((td, index) => {
+                    trow[`${headers[index]}`] = td.textContent.trim()
+                })
+                allData.push(trow)
             })
-            allData.push(trow)
-        })
-        return allData;
-    });
-    console.log('data: ', data);
+            return allData;
+        });
+        return data;
+    }
+
+    let tableData = []
+    let masterData = []
+    const selector = '.ntlist li';
+    await page.waitForSelector(selector);
+    const list = await page.$$(selector)
+    for (const liElement of list) {
+        console.log('liElement: ', liElement);
+        if (liElement) {
+            await liElement.click();
+            await new Promise(resolve => setTimeout(() => { resolve(true) }, 5000));
+            // tableData = await getTableData()
+            // masterData.push({
+            //     [`${liElement.textContent.trim()}`]: tableData
+            // })
+        }
+    }
+
 })();

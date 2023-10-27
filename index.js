1180b5c15a7dad00bfee9550c9f00c0250053485
kishorelal <kishoreag@cloudix.io>
2023-10-27 16:54:03 +0530

    in progress

--- index.js
+++ index.js
@@ -1,50 +1,78 @@
 import puppeteer from "puppeteer";
+import fs from 'fs/promises';
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--start-maximized'] });
+const browser = await puppeteer.launch({ headless: "new", defaultViewport: null, args: ['--start-maximized'] });
+
+const getIndexData = async (url) => {
     const page = await browser.newPage();
-
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.evaluate(() => {
-        document.body.style.width = '100%';
-    });
-    await page.goto('https://www.moneycontrol.com/markets/indian-indices/');
-    // await page.goto('https://pptr.dev/api/puppeteer.page.waitfornavigation/');
-    const getTableData = async () => {
-        console.log("vanten")
-        const data = await page.evaluate(() => {
-            let allData = []
-            const table = document.querySelector('#indicesTableData');
-            const headers = Array.from(table.querySelectorAll('thead th')).map((td) => td.textContent.trim());
-            const rows = Array.from(table.querySelectorAll('tbody tr'));
-            rows.map((tr) => {
-                const cells = Array.from(tr.querySelectorAll('td'));
-                let trow = {}
-                cells.map((td, index) => {
-                    trow[`${headers[index]}`] = td.textContent.trim()
-                })
-                allData.push(trow)
+    await page.goto(url);
+    await page.waitForSelector(`#indicesTable tbody tr`);
+    const indexDetails = await page.$$eval(
+        "#indicesTable tbody tr",
+        (rows) =>
+            rows.map((row) => {
+                if (row.querySelector("td:nth-child(2)")) {
+                    return {
+                        name: row.querySelector("td").textContent.trim(),
+                        lastTradedPrice: row.querySelector("td:nth-child(2)").textContent.trim(),
+                        change_percentage:
+                            row.querySelector("td:nth-child(3)").textContent.trim(),
+                        volume: row.querySelector("td:nth-child(4)").textContent.trim(),
+                        buy_price: row.querySelector("td:nth-child(5)").textContent.trim(),
+                        sell_price: row.querySelector("td:nth-child(6)").textContent.trim(),
+                        buy_qty: row.querySelector("td:nth-child(7)").textContent.trim(),
+                        sell_qty: row.querySelector("td:nth-child(8)").textContent.trim(),
+                    };
+                } else {
+                    return {
+                        name: '',
+                        lastTradedPrice: '',
+                        change_percentage: '',
+                        volume: '',
+                        buy_price: '',
+                        sell_price: '',
+                        buy_qty: '',
+                        sell_qty: '',
+                    }
+                }
             })
-            return allData;
-        });
-        return data;
-    }
+    );
+    page.close();
+    return indexDetails;
+}
 
-    let tableData = []
-    let masterData = []
-    const selector = '.ntlist li';
-    await page.waitForSelector(selector);
-    const list = await page.$$(selector)
-    for (const liElement of list) {
-        console.log('liElement: ', liElement);
-        if (liElement) {
-            await liElement.click();
-            await new Promise(resolve => setTimeout(() => { resolve(true) }, 5000));
-            // tableData = await getTableData()
-            // masterData.push({
-            //     [`${liElement.textContent.trim()}`]: tableData
-            // })
-        }
-    }
+const getIndicesData = async (page) => {
+    return await page.$$eval('.ntlist li', (indices) => indices.map((i) => ({
+        name: i.textContent.trim(),
+        url: i.querySelector('h2 a').href
+    })));
+}
 
-})();
+const saveAsJson = async (data) => {
+    try {
+        await fs.writeFile('indices.json', JSON.stringify(data, null, 4));
+        console.log("File written succesfully")
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const initialize = async () => {
+    console.log("======= Scrapping Started =======")
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.evaluate(() => document.body.style.width = '100%');
+    await page.goto('https://www.moneycontrol.com/markets/indian-indices/');
+    let masterData = {};
+    const indices = await getIndicesData(page);
+    console.log("Retrived Indices Data")
+    for (let i = 0; i < indices.length; i++) {
+        masterData[indices[i].name] = await getIndexData(indices[i].url)
+        console.log(`Scrapping - ${indices[i].name} data`)
+    }
+    await saveAsJson(masterData)
+    console.log("======= Scrapping Finished =======")
+    await browser.close()
+}
+
+initialize();
